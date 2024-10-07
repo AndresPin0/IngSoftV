ccf7aba4e8886d139d04d243587474c1674c610c
Andrés Pino <andres@Andress-MacBook-Pro.local>
2024-10-07 08:28:41 -0500

    other zip

---
+++ saamfi2-backend/saamfi-logic/src/test/java/co/edu/icesi/dev/saamfi/logic/authentication/IntegrationAutUserpersonServiceImplTest.java
@@ -0,0 +1,16 @@
+package co.edu.icesi.dev.saamfi.logic.authentication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+@Transactional
+@SpringBootTest
+public class IntegrationAutUserpersonServiceImplTest {
+
+	@Test
+	public void getUsersbyPerson() throws Exception {
+
+	}
+
+}
